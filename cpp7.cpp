951338f1ee2d582594f4dff7dd0bda14df8344db
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-01-18 20:57:36 +0330

    Add files via upload

---
+++ cpp7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+void sayhello(string name){
+  cout<<"hello"<<name<<endl;
+}
+int main(){
+	string name;
+    	cin>>name;
+        	sayhello(name);
+	
+}
+ //sayhellotothename
+
