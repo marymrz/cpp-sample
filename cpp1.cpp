a0596d74be1ab6546d9a95dec0fdfdebe0e19ae6
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-01-25 17:53:09 +0330

    Add files via upload

---
+++ cpp1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int x,i;
+	char max;
+	cin>>x;
+	while(i<3){
+		i=i+1;
+	}
+	if(max<x,max=x){
+cout<<max;
+}
+getch();
+	
+}
