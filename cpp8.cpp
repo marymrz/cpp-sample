b469bb80211c9bf965bbb8d44038abf52b81de1b
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-01-25 17:58:14 +0330

    Add files via upload

---
+++ cpp8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+#include <cmath>
+using namespace std;
+int main(){
+	int x=1,y=1,z,k,n;
+	   cout<<"how many number for fibonachi :";
+	    	cout<<x<<y;
+        	int i=2;
+               while(i<n){
+               	z=x+y;
+               	cout<<z;
+               	x=y;
+               	y=z;
+               	i++;
+			   }
+               
+          getch();
+}
