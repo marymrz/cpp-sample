109278f277a33afd7566ea4bb3903557d3153ae8
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-01-25 18:57:50 +0330

    Add files via upload

---
+++ cpp14.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+int i;
+ for(i=0;i<=20;i++){
+   if(i%2==0){
+   	 cout<<"zoj="<<i<<"\n";
+   }
+       else{
+       	 cout<<"fard="<<i<<"\n";
+	   }
+}
+getch();
+}
