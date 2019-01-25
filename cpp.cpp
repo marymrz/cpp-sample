71300a21c182f3fe0d7226b01fb2067c39a3255c
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-01-25 17:51:13 +0330

    Add files via upload

---
+++ cpp.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int num;
+	 cout<<"enter the number to check the day of week:";
+	 cin>>num;
+	 switch(num)
+	 {
+	 	case 1 : cout<<"it is saturday";
+	 	break;
+	 	case 2 : cout<<"it is sunday";
+	 	break;
+	 	case 3 : cout<<"it is monday";
+	 	break;
+	 	case 4 : cout<<"it is tuesday";
+	 	break;
+	 	case 5 : cout<<"it is wednesday";
+	 	break;
+	 	case 6 : cout<<"it is thursday";
+	 	break;
+	 	case 7: cout<<"it is friday";
+	 	break;
+	 	default: cout<<"this is a wrong number";
+	 }
+	 getch();
+	 }
+
+
