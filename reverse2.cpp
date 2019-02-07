c9af532a3de293258080735a04507697e66e725e
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-02-08 00:09:18 +0330

    Add files via upload

---
+++ reverse2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+main(){
+	const int arraysize=5;
+	int a[arraysize]={0,1,12,3,14};
+	
+		for(int i=arraysize-1 ; i>=0  ;i--)
+		cout<<a[i]<<endl;
+		cout<<"***reverse***\n";
+	}
