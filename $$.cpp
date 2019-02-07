7b12f2b406187beaff9be7d424d1d61ac6306e8c
Maryam mirzakhani <46631619+marymrz@users.noreply.github.com>
2019-02-08 00:06:40 +0330

    Add files via upload

---
+++ $$.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	int i,j;
+	for(i=0 ; i<4 ; i++){
+	
+	for(j=0 ; j<6 ; j++){
+	
+	cout<<'$';
+}
+	cout<<"\n";
+}
+getch();
+}
+
